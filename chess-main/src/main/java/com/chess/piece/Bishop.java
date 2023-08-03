6babc1ef0ed2842c489eb77175f7fc976c41e68a
Nazarenko Daniil <99690828+daniilRS@users.noreply.github.com>
2023-08-03 19:24:08 +0300

    Add files via upload

---
+++ chess-main/src/main/java/com/chess/piece/Bishop.java
@@ -0,0 +1,16 @@
+package com.chess.piece;
+
+import com.chess.Color;
+import com.chess.Coordinates;
+import java.util.Set;
+
+public class Bishop extends LongRangePiece implements IBishop {
+    public Bishop(Color color, Coordinates coordinates) {
+        super(color, coordinates);
+    }
+
+    @Override
+    protected Set<CoordinatesShift> getPieceMoves() {
+        return getBishopMoves();
+    }
+}
---
+++ chess-main/src/main/java/com/chess/piece/Rook.java
@@ -0,0 +1,16 @@
+package com.chess.piece;
+
+import com.chess.Color;
+import com.chess.Coordinates;
+import java.util.Set;
+
+public class Rook extends LongRangePiece implements IRook {
+    public Rook(Color color, Coordinates coordinates) {
+        super(color, coordinates);
+    }
+
+    @Override
+    protected Set<CoordinatesShift> getPieceMoves() {
+        return getRookMoves();
+    }
+}
